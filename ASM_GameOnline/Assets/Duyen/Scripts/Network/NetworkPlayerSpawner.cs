6acfc4fe4b82bbe6bc9719ec318b420d10dd6bca
Duyen Nguyen <davichannel023@gmail.com>
2025-04-13 23:16:47 +0700

    npc move,attack

---
+++ ASM_GameOnline/Assets/Duyen/Scripts/Network/NetworkPlayerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class NetworkPlayerSpawner : SimulationBehaviour, IPlayerJoined
+{
+    public GameObject playerPrefab;
+    public GameObject npcPrefab;
+    public float npcSpawnOffset = 1.5f; // Khoảng cách spawn NPC so với player
+
+    public void PlayerJoined(PlayerRef player)
+    {
+        if (player == Runner.LocalPlayer)
+        {
+            var spawnPos = new Vector3(Random.Range(-5f, -4f), 0, 0);
+
+            // Spawn player
+            var playerObj = Runner.Spawn(playerPrefab, spawnPos, Quaternion.identity, player);
+
+            // Spawn NPC cho player này (vị trí bên phải player)
+            var npcPos = new Vector3(spawnPos.x + npcSpawnOffset, spawnPos.y, spawnPos.z);
+            Runner.Spawn(npcPrefab, npcPos, Quaternion.identity, player, (runner, o) =>
+            {
+                // Thiết lập player mà NPC sẽ theo
+                o.GetComponent<NPC>().SetTarget(playerObj.transform);
+            });
+        }
+    }
+}
